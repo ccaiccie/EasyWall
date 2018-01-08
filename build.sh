597a373b58a709a2417ed2b3087ccd0aa4687d17
Jochen Pylypiw <jochen@pylypiw.com>
2018-01-08 15:08:12 +0100

    Gitignore, Readme, Version and Build Tools

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 SCRIPTPATH=$(pwd)
 SRCPATH=$SCRIPTPATH/src
 GOPATH=/usr/local/go/bin/go
+GRUNTPATH=$SRCPATH/node_modules/grunt/bin/grunt
+CONFIGPATH=$SCRIPTPATH/config
 
+rm -rf $CONFIGPATH
 cd $SRCPATH
 $GOPATH build
+$GRUNTPATH
 mv src easywall
 chmod +x easywall
 mv easywall $SCRIPTPATH
